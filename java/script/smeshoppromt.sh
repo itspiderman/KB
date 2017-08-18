f9ff303dea89c7ed955a5a8af4b703789e98d83b
itspiderman <itman020@126.com>
2017-08-18 13:49:09 +0800

    Create smeshoppromt.sh
    draft 01

---
+++ java/script/smeshoppromt.sh
@@ -0,0 +1,24 @@
+#promot script, cd to class root path
+cd /usr/local/tomcat/webapps/smeshop/WEB-INF/classes
+#1. com.smeshop.pojo
+cd com/smeshop/pojo
+wget WXMessage.java
+#cd to class root path
+cd ..
+cd ..
+cd ..
+#2. com.smeshop.servlet
+cd com.smeshop.servlet
+wget WeiXinServlet.java
+#cd to class root path
+cd ..
+cd ..
+cd ..
+#3.com.smeshop.util
+cd com/smeshop/util
+wget CheckUtil.java
+wget Message.java
+#cd to class root path
+cd ..
+cd ..
+cd ..
